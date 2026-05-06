73151803a6528cb26b39cde0f4fb041aaf43d08d
Curtis Bradley <curtisbradley822@gmail.com>
2026-05-06 00:52:20 -0700

    Fixed Routing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,22 @@
 import express from "express";
+import path from "path";
 
-const app = express()
-const port = 3000
-const STATIC_DIR = '../frontend/dist'
+const app = express();
+const port = 3000;
 
+const STATIC_DIR = path.resolve(__dirname, "../../frontend/dist");
+const PUBLIC_DIR = path.resolve(__dirname, "../../frontend/public");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static(PUBLIC_DIR));
 app.use(express.static(STATIC_DIR));
 
-app.get('/', (req , res ) => {
-    res.send('Hello World!')
-})
+app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(STATIC_DIR, "index.html"));
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
